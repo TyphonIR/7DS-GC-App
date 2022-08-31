9dfefe80a435bcc9ca8edd747612e9ebf4f2dd6e
TyphonIR <90115572+TyphonIR@users.noreply.github.com>
2022-08-31 16:38:43 +0200

    file changes

--- app files/src/translate/global/shortcuts.js
+++ app files/src/translate/global/shortcuts.js
@@ -1,9 +1,9 @@
 // Keyboard Shortcuts
 window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && helpSearchSection.style.width !== "0") toggleHelpSideBar("close");
+    if (e.key === "Escape" && helpSideBar_div.style.width !== "0") toggleHelpSideBar("close");
     if (e.key === "Shift") {
-        if (current_file_name === "search.html") search_input.focus();
-        else if (current_file_name === "convert.html") convert_input.focus();
+        if (current_file_name === "search.html" && search_input !== document.activeElement) search_input.focus();
+        else if (current_file_name === "convert.html" && convert_input !== document.activeElement) convert_input.focus();
     }
     if (e.ctrlKey && e.key === "Alt") {
         toggleMode();
